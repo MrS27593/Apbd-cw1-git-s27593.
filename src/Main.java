056a0c124892debc13a3fa33e8a1e3853760fb67
Marcin Zabkowski <sXXXX@pjwstk.edu.pl>
2026-03-18 18:29:47 +0100

    In main class I created method which count the average of whole intArray.There is also an example

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-
+        int[] a = {1,213,1,23,5,6};
+        int average = CalculateAverage(a);
+        System.out.println(average);
+    }
+    public static int CalculateAverage(int[] a){
+        int score = 0;
+        for (int i = 0; i < a.length ; i++)
+            score += a[i];
+        score/=a.length;
+        return score;
     }
 }
