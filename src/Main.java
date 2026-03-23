bf0c6f630802da64469b5431ee1c63e25e9bd3d3
Marcin Zabkowski <sXXXX@pjwstk.edu.pl>
2026-03-23 17:25:33 +0100

    aded for in min function

--- src/Main.java
+++ src/Main.java
@@ -28,5 +28,8 @@ public class Main {
     public static int CalculateMin(int[] values){
         int min = 0;
         return min;
+        for(int i = 0 ; i < values.length ; i++){
+            
+        }
     }
 }
