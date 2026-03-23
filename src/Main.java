4fd340d4e958cf4afa651c45875aa1e18570ceb4
Marcin Zabkowski <sXXXX@pjwstk.edu.pl>
2026-03-23 17:15:15 +0100

    first update in min function

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
         int[] a = {1,213,1,23,5,6};
         int average = CalculateAverage(a);
         System.out.println(average);
+        int max = CalculateMax(a);
+        System.out.println(max);
     }
     public static int CalculateAverage(int[] a){
         int score = 0;
@@ -10,5 +12,21 @@ public class Main {
             score += a[i];
         score/=a.length;
         return score;
+    }
+    public static int CalculateMax(int[] values){
+        int max = 0;
+        int left= 0;
+        for(int i = 1; i < values.length ; i++){
+            if(values[i] > max)
+                max = values[i];
+
+            left+=1;
+        }
+        return max;
+    }
+
+    public static int CalculateMin(int[] values){
+        int min = 0;
+        return min;
     }
 }
